c79c47e6ab7a9b40d3cba68d08b8bc91e0e809b1
Alex Chavkin <achavkin@flatironinstitute.org>
2018-09-17 17:53:30 -0400

    2-6: cleaner code

--- exercise-2-6.c
+++ exercise-2-6.c
@@ -8,18 +8,17 @@ int main(){
 	unsigned int y=		0b100110001;
 	unsigned int result=	0b110000111;
 	printf("Expected result for setbits(x,4,2,y): %d\n",result);
-	printf("%d\n",setbits(x,4,2,y));
+	printf("Actual result for setbits(x,4,2,y):   %d\n",setbits(x,4,2,y));
 	return 0;
 
 }
 
 int setbits(unsigned int x, int n, int p, unsigned int y){
 
-	unsigned int mask;
-	mask = ((1<<n)-1)<<p;
-	x= x & ~mask;
-	y= (y<<p) & mask;
-	x+=y;
+	unsigned int mask = ((1<<n)-1)<<p;
+	x &= ~mask;
+	y = (y<<p) & mask;
+	x += y;
 	return x;
 	
 }
