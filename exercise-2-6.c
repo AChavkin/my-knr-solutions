0549fc15b9aab1f6f213d5e4b2995b4a6bdd8c75
Alex Chavkin <39803810+AChavkin@users.noreply.github.com>
2018-09-17 15:35:51 -0400

    Add exercise-2-6.c

---
+++ exercise-2-6.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int setbits(unsigned int x, int n, int p, unsigned int y);
+
+int main(){
+
+	unsigned int x= 	0b110100111;
+	unsigned int y=		0b100110001;
+	unsigned int result=	0b110000111;
+	printf("Expected result for setbits(x,4,2,y): %d\n",result);
+	printf("%d\n",setbits(x,4,2,y));
+	return 0;
+
+}
+
+int setbits(unsigned int x, int n, int p, unsigned int y){
+
+	unsigned int mask;
+	mask = ((1<<n)-1)<<p;
+	x= x & ~mask;
+	y= (y<<p) & mask;
+	x+=y;
+	return x;
+	
+}
