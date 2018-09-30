5c5f0662ec8813099f1ca1a62d8daf2ec0e35beb
Alex Chavkin <achavkin@flatironinstitute.org>
2018-09-30 14:52:43 -0400

    exercise-3-02: add jail function

--- Chapter3/exercise-3-02.c
+++ Chapter3/exercise-3-02.c
@@ -3,6 +3,7 @@
 #define SIZE 50
 
 void escape(char s[], char t[]);
+void jail(char s[], char t[]);
 
 int main(){
 
@@ -11,7 +12,13 @@ int main(){
 
 	escape(s, t);
 
+	char a[SIZE];
+	char b[] = "\\t \\n \\a \\? \\\'\\n\\\"\\?\\t\\a\\\'\\n";
+
+	jail(a,b);
+
 	printf("%s\n", s);
+	printf("%s\n", a);
 
 	return 0;
 }
@@ -37,6 +44,38 @@ void escape(char s[], char t[]){
 			case '\"': s[j]='\\'; s[++j]='\"'; break;
 			default: s[j]=t[i]; break;
 		}
+		++j;
+	}
+	s[j]='\0';
+}
+
+void jail(char s[], char t[]){
+
+	int i,j;
+	j=0;
+
+	for(i=0; t[i]!='\0'; ++i){
+
+		if (t[i]=='\\'){
+
+			switch(t[++i]){
+
+			case 't': s[j]='\t'; break;
+			case 'n': s[j]='\n'; break;
+			case 'a': s[j]='\a'; break;
+			case 'b': s[j]='\b'; break;
+			case 'f': s[j]='\f'; break;
+			case 'r': s[j]='\r'; break;
+			case 'v': s[j]='\v'; break;
+			case '\?': s[j]='\?'; break;
+			case '\'': s[j]='\''; break;
+			case '\"': s[j]='\"'; break;
+			case '\\': s[j]='\\'; break;
+			default: break;
+
+			}
+		}
+		else s[j]=t[i];
 		++j;
 	}
 	s[j]='\0';
