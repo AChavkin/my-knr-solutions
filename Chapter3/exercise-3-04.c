982b09ebea12bee50f799cdbcde2961b1d33b5dc
Alex Chavkin <achavkin@flatironinstitute.org>
2018-10-05 18:20:02 -0400

    add 3-04

---
+++ Chapter3/exercise-3-04.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <string.h>
+#include <limits.h>
+
+#define SIZE 100
+
+void itoa(int n, char s[]);
+void reverse(char s[]);
+
+/* The original itoa() doesn't deal with the minimum value for a signed int correctly because there
+isn't space for the positive version (absolute value) to be stored in an int variable. Since this
+isjust a single possible case, I remedied this by just checking for said case, noting
+it, adding one, and checking back after the conversion was completed, then adding one to the first
+digit of the string (last digit of input int). This digit can never be 9 for the minimum value and break
+since (2^n)-1 is never 9. */
+
+int main(){
+
+	char s[SIZE];
+	char t[SIZE];
+	itoa(INT_MAX-UINT_MAX, s);
+	itoa(3748234, t);
+	printf("%s\n", s);
+	printf("%s\n", t);
+	return 0;
+}
+
+void itoa(int n, char s[]){
+
+	int i, sign, flag;
+	i = sign = flag = 0;
+
+	if (n == INT_MAX-UINT_MAX) {
+		flag=1;
+		++n;
+	}
+	if ((sign = n) < 0) n = -n;
+	do{
+		s[i++] = n % 10 + '0';
+		printf("CURRENT N: %d\t CURRENT S: %s\n", n, s);
+	} while ((n /= 10) > 0);
+	if (sign < 0) s[i++] = '-';
+	s[i] = '\0';
+	if (flag==1) ++s[0];
+	reverse(s);
+}
+
+void reverse(char s[]){
+
+	int c, i, j;
+
+	for (i=0, j=strlen(s)-1; i<j; i++, j--){
+		c = s[i];
+		s[i] = s[j];
+		s[j] = c;
+	}
+}
