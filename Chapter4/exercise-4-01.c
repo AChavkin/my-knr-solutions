9b6d0c83217a4ee9f5853c813834b25b420d7aef
Alex Chavkin <achavkin@flatironinstitute.org>
2018-10-07 12:07:30 -0400

    cut a line out of 4-01, add another test

--- Chapter4/exercise-4-01.c
+++ Chapter4/exercise-4-01.c
@@ -9,8 +9,10 @@ int main(){
 	char s[] = "If only the world was a kinder place. "
 	"Alas, the caretakers of the world are not kind.";
 	char t[] = "world";
+	char u[] = "gothic";
 
-	printf("%d\n",strrindex(s,t));
+	printf("%d\n", strrindex(s,u));
+	printf("%d\n", strrindex(s,t));
 
 	return 0;
 }
@@ -20,13 +22,12 @@ int strrindex(char s[], char t[]){
 	// Return the index of the last (rightmost) match of t in s.
 
 	int i, j, k, match;
-	match=0;
+	match=-1;
 
 	for (i=0; s[i]!='\0'; i++){
 		for (j=i, k=0; t[k]!='\0' && s[j]==t[k]; j++, k++)
 			;
 		if (k>0 && t[k]=='\0') match=i;
 	}
-	if (match) return match;
-	return -1;
+	return match;
 }
