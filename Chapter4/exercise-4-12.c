e9b6bcba25f2a878ceff2f65a7348ded90708987
Alex Chavkin <achavkin@flatironinstitute.org>
2018-10-15 16:02:12 -0400

    add 4-12

---
+++ Chapter4/exercise-4-12.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <limits.h>
+
+#define SIZE 100
+
+void itoa(int n, char s[]);
+void reverse(char s[]);
+
+int main()
+{
+
+	char test[SIZE];
+	itoa(-2134567, test);
+	printf("%s\n", test);
+	return 0;
+}
+
+void itoa(int n, char s[])
+{	
+	static int i = 0;
+	printf("LEN %d\n STR: %s\n N: %d\n", i, s, n);
+	int sign, mod;
+
+	sign = n;
+	
+	s[i++] = abs(n % 10) + '0';
+	if (n /= 10) itoa(n, s);
+	else {
+		if (sign < 0) s[i++] = '-';
+		s[i] = '\0';
+		reverse(s);
+	}
+}
+
+void reverse(char s[])
+{
+	int c, i, j;
+
+	for (i=0, j=strlen(s)-1; i<j; i++, j--){
+		c = s[i];
+		s[i] = s[j];
+		s[j] = c;
+	}
+}
