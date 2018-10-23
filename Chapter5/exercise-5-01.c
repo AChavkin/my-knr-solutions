9812ae921fd44a18950b6abbf01e1c7c154a2aa0
Alex Chavkin <achavkin@flatironinstitute.org>
2018-10-23 17:10:10 -0400

    add 5-01

---
+++ Chapter5/exercise-5-01.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <ctype.h>
+
+#define BUFSIZE 100
+#define SIZE 100
+
+int getch(void);
+void ungetch(int c);
+int getint(int *pn);
+
+int main()
+{
+	int c, n, res, a[SIZE];
+
+	for (n = 0; n < SIZE && (res=getint(&a[n])) != EOF; n++)
+		;
+	for (n = 0; n < SIZE; n++) printf("%d\n", a[n]);
+
+	return 0;
+}
+
+int getint(int *pn)
+{
+	int c, d, sign;
+
+	while (isspace(c=getch()))
+		;
+	if (!isdigit(c) && c != EOF && c != '+' && c != '-'){
+		ungetch(c);
+		return 0;
+	}
+	sign = (c == '-') ? -1 : 1;
+	if (c == '+' || c == '-'){
+		d = c;
+		if (!isdigit(c=getch())){
+			ungetch('a');
+			return 0;
+		}
+	}
+	for (*pn = 0; isdigit(c); c = getch())
+		*pn = 10 * *pn + (c - '0');
+	*pn *= sign;
+	if (c != EOF) ungetch(c);
+	return c;
+}
+
+char buf[BUFSIZE];
+int bufp = 0;
+
+int getch(void)
+{
+	return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c)
+{
+	if (bufp >= BUFSIZE) printf("ungetch: too many characters\n");
+	else buf[bufp++] = c;
+}
